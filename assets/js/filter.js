d7587af73298c0ba1d18bc3d76ce72bbf5fb6fa8
ziqian1125 <71480117+ziqian1125@users.noreply.github.com>
2021-05-16 00:54:43 +0800

    add search fuctionality for the tutorial page

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -13,3 +13,19 @@ function myFunction() {
         }
     }
 }
+
+function myFunction2() {
+    var input, filter, cards, cardContainer, h4, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("myItems");
+    cards = cardContainer.getElementsByClassName("dom1");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector("h6");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
